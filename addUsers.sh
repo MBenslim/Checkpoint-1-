41f0c8e07df1e41d3c0baa2ba3933640fe11b793
MBenslim <mbe5610@gmail.com>
2025-04-04 12:44:06 +0200

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,1 +1,24 @@
+#! /bin/bash
+# Mohamed 
+# Création d'utilisateur
 
+read -p "Nom d'utilisateur : " username
+
+if  cat /etc/passwd | grep -q "^$username:"
+ then
+    echo "L'utilisateur $username existe déjà."
+    exit 1
+fi
+if [ -z "$username" ]
+ then
+    echo "Il manque les noms d'utilisateur en argument - Fin du script."
+    exit 1
+fi
+
+if id "$username" > /dev/null 2> /dev/null
+ then
+    echo "L'utilisateur $username existe déjà."
+else
+    sudo useradd -m -s /bin/bash "$username"
+    echo "L'utilisateur $username a été créé avec succès."
+fi
